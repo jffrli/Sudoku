5dc3f4666c3ec9aa2116bca218f1e82f8c0b0b44
Jeffrey Li <jffr.li1@gmail.com>
2018-12-18 23:24:32 -0800

    Basic outlines for SudokuBoard and SudokuGame

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -8,9 +8,49 @@ All functions are just ideas and subject to changes
 public class SudokuBoard {
 	SudokuSquare[][] board;
 
-
+	/*
+	Constructor
+	TODO
+	*/
 	public SudokuBoard() {
 		board = new SudokuSquare[3][3];
+	}
+
+
+	/*
+	Adds value to position
+	*/
+	public void addVal(int x, int y, int value) {
+
+	}
+
+
+	/* Checks if a value is in a given row
+	TODO
+	*/
+	public boolean checkRow(int row, int value) {
+
+	}
+
+	/* Checks if a value is in a given column
+	TODO
+	*/
+	public boolean checkCol(int col, int value) {
+
+	}
+
+	/* Empties all spaces of the board
+	TODO
+	*/
+	public void resetBoard() {
+
+	}
+
+	/* String representation of the board
+		TODO
+	*/
+	public String toString() {
+
 	}
 
 
---
+++ SudokuGame.java
@@ -0,0 +1,71 @@
+/*
+Most of the actual stuff will be here
+Algorithms and such
+Takes minimal input from interface
+
+--------------------------------------------------------------------------------------------
+All return types and parameters are temporary and subject to change when actual coding starts
+All functions are just ideas and subject to changes
+*/
+public class SudokuGame {
+
+	/* Constructor
+	TODO
+	*/
+	public SudokuGame() {
+
+	}
+
+	/* Gets values inside board
+	TODO
+	*/
+	public void showBoard() {
+
+	}
+
+	/*
+	Adds value to position
+	TODO
+	*/
+	public void addValue(int x, int y, int value) {
+
+	}
+
+	/* Solves the puzzle
+	TODO
+	*/
+	public void solve() {
+
+	}
+
+	/* Generates a puzzle
+	TODO
+	*/
+	public void generatePuzzle() {
+
+	}
+
+	/*Empties the board
+	TODO
+	*/
+	public void resetBoard() {
+
+	}
+
+	/* Resets the puzzle (removes all user inputted values)
+	TODO
+	*/
+	public void resetPuzzle() {
+
+	}
+
+	/* Checks if current progress is valid
+	Checks for obvious contradictions
+	Also used to check if solution works
+	TODO
+	*/
+	public void checkProg() {
+
+	}
+
+}
